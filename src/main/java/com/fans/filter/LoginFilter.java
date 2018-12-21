67ebc80ac81f03d74c42a2d0ba9ed05838642306
F-Kapok <5219824@qq.com>
2018-12-21 21:00:21 +0800

    update

--- src/main/java/com/fans/filter/LoginFilter.java
+++ src/main/java/com/fans/filter/LoginFilter.java
@@ -52,7 +52,6 @@ public class LoginFilter implements Filter {
         RequestHolder.add(user);
         RequestHolder.add(request);
         filterChain.doFilter(servletRequest, servletResponse);
-        return;
     }
 
     @Override
