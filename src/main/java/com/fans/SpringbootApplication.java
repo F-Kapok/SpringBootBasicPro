ae44ee4b372b2160573cab0ce00a7fafb9ddad17
F-Kapok <5219824@qq.com>
2018-12-22 09:46:30 +0800

    update

--- src/main/java/com/fans/SpringbootApplication.java
+++ src/main/java/com/fans/SpringbootApplication.java
@@ -3,6 +3,7 @@ package com.fans;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @Description: TODO 入口
