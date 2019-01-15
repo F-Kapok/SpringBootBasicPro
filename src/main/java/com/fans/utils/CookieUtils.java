6ff25d1b5575fbff26ee3a5f360cbff0c816fdc1
范凯 <45064973+F-Kapok@users.noreply.github.com>
2019-01-15 18:00:08 +0800

    Update CookieUtils.java

--- src/main/java/com/fans/utils/CookieUtils.java
+++ src/main/java/com/fans/utils/CookieUtils.java
@@ -222,10 +222,10 @@ public class CookieUtils {
             int len = domains.length;
             if (len > 3) {
                 // www.xxx.com.cn
-                domainName = "." + domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
+                domainName = domains[len - 3] + "." + domains[len - 2] + "." + domains[len - 1];
             } else if (len <= 3 && len > 1) {
                 // xxx.com or xxx.cn
-                domainName = "." + domains[len - 2] + "." + domains[len - 1];
+                domainName = domains[len - 2] + "." + domains[len - 1];
             } else {
                 domainName = serverName;
             }
