3756db69b5860d21fdb99f28f79b3e94657c238d
F-Kapok <5219824@qq.com>
2019-03-04 16:01:50 +0800

    update“

--- src/main/java/com/fans/config/DruidConfiguration.java
+++ src/main/java/com/fans/config/DruidConfiguration.java
@@ -11,8 +11,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 
 /**
  * @ClassName DruidConfiguration
