f243c44232e9f1680d7eec38226cd6b0cb99a0e2
F-Kapok <5219824@qq.com>
2019-08-03 22:24:11 +0800

    update

--- src/main/java/com/fans/common/RedisSynchronize.java
+++ src/main/java/com/fans/common/RedisSynchronize.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
@@ -23,16 +24,20 @@ public class RedisSynchronize {
 
     /**
      * @Description: 加锁
-     * @Param: [key, time]
-     * @return: boolean
+     * @Param: [key, time(当前时间+超时时间)]
+     * @return: boolean 锁住返回true
      * @Author: fan
      * @Date: 2019/01/16 13:22
      **/
+    @Transactional(rollbackFor = Exception.class)
     public boolean lock(String key, String time) {
+        //setIfAbsent相当于setNX 返回boolean 不存在就直接上锁
         if (redisTemplate.opsForValue().setIfAbsent(key + SUFFIX, time)) {
             return true;
         }
+        //存在则取出超时时间
         String currentTime = redisTemplate.opsForValue().get(key + SUFFIX);
+        //如果锁超时 取出上一个锁的时间并更新时间（这里可以保证N个线程同时进来肯定会有一个线程拿到锁）
         if (StringUtils.isNotBlank(currentTime) && Long.parseLong(currentTime) < System.currentTimeMillis()) {
             String oldTime = redisTemplate.opsForValue().getAndSet(key + SUFFIX, time);
             if (StringUtils.isNotBlank(oldTime) && oldTime.equals(currentTime)) {
